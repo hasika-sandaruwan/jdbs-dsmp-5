54f6cc91b5e81af92662468e53c2fff06f4e4380
education-bootcamp <education.seekerscloud@gmail.com>
2025-05-10 10:41:35 +0530

    1

--- src/controller/CustomerFormController.java
+++ src/controller/CustomerFormController.java
@@ -1,9 +1,12 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import model.Customer;
+import util.CrudUtil;
 
 public class CustomerFormController {
     public TextField txtId;
@@ -18,6 +21,37 @@ public class CustomerFormController {
     public TableColumn colTools;
 
     public void saveCustomerOnAction(ActionEvent actionEvent) {
+        Customer customer = new Customer(
+                txtId.getText(),
+                txtName.getText(),
+                txtAddress.getText(),
+                Double.parseDouble(txtSalary.getText())
+        );
+        try{
+            boolean isSaved = CrudUtil.execute(
+                    "INSERT INTO customer VALUES (?, ?, ?, ?)",
+                    customer.getId(),
+                    customer.getName(),
+                    customer.getAddress(),
+                    customer.getSalary()
+            );
+            if(isSaved){
+                clearFields();
+                new Alert(Alert.AlertType.INFORMATION, "Customer Saved").show();
+            }else{
+                new Alert(Alert.AlertType.WARNING, "Customer Not Saved").show();
+            }
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void clearFields(){
+        txtId.clear();
+        txtName.clear();
+        txtAddress.clear();
+        txtSalary.clear();
     }
 
     public void newCustomerOnAction(ActionEvent actionEvent) {
